131aad0d584468532715b7bfbdf07ceedd32e38c
Olga Slazarska <slazarska@protonmail.com>
2021-07-21 20:13:10 +0300

    fixed RekrytointiPageTests and MainPageTests

--- src/test/java/fi/comiq/tests/tests/MainPageTests.java
+++ src/test/java/fi/comiq/tests/tests/MainPageTests.java
@@ -1,46 +1,45 @@
 package fi.comiq.tests.tests;
 
 import fi.comiq.tests.pages.MainPage;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MainPageTests extends TestBase {
 
-    static MainPage mainPage = new MainPage();
-
-    @BeforeAll
-    static void openMain() {
-        mainPage.openMainPage();
-    }
+    MainPage mainPage = new MainPage();
 
     @Test
     @DisplayName("Open main page")
     void openMainPage() {
+        mainPage.openMainPage();
         mainPage.checkMainPageIsOpened();
     }
 
     @Test
     @DisplayName("Main page title should have header text")
     void checkTitle() {
+        mainPage.openMainPage();
         mainPage.checkTitle();
     }
 
     @Test
     @DisplayName("Page console log should not have errors")
     void checkConsoleLogHaveNoErrors() {
+        mainPage.openMainPage();
         mainPage.checkConsoleLogHaveNoErrors();
     }
 
     @Test
     @DisplayName("Default language for main page should be finnish")
     void checkPageIsOnFinnishLangByDefault() {
+        mainPage.openMainPage();
         mainPage.checkPageIsInFinn();
     }
 
     @Test
     @DisplayName("Switch to english language is available")
     void checkSwithToEngLang() {
+        mainPage.openMainPage();
         mainPage.chooseEnglsihLanguage();
         mainPage.checkPageIsInEng();
     }
--- src/test/java/fi/comiq/tests/tests/RekrytointiPageTests.java
+++ src/test/java/fi/comiq/tests/tests/RekrytointiPageTests.java
@@ -2,30 +2,27 @@ package fi.comiq.tests.tests;
 
 import fi.comiq.tests.pages.MainPage;
 import fi.comiq.tests.pages.RekrytointiPage;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RekrytointiPageTests {
 
-    static MainPage mainPage = new MainPage();
-    static RekrytointiPage rekrytointiPage = new RekrytointiPage();
-
-    @BeforeAll
-    static void openMain() {
-        mainPage.openMainPage();
-        rekrytointiPage.openRekrytointiPage();
-    }
+    MainPage mainPage = new MainPage();
+    RekrytointiPage rekrytointiPage = new RekrytointiPage();
 
     @Test
     @DisplayName("Open Rekrytointi Page")
     void openRekrytointiPage() {
+        mainPage.openMainPage();
+        rekrytointiPage.openRekrytointiPage();
         rekrytointiPage.checkRekrytointiPageIsOpened();
     }
 
     @Test
     @DisplayName("Check the list of vacancies has AQA vacancy ")
     void checkTheVacancyForAQA() {
+        mainPage.openMainPage();
+        rekrytointiPage.openRekrytointiPage();
         rekrytointiPage.checkTestAutomationExpertVacancy();
     }
 }
